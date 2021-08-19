d3c51b700cf8966720717c88dfc0b1dcd50596da
Bryan Vincent <bvince77@gmail.com>
2021-08-18 20:38:18 -0400

    fixed navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,20 +2,21 @@ import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
+
 const Header = () => {
     return (
         <header>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" class="nav">
                 <Container>
                     <LinkContainer to='/'>
                         <Navbar.Brand>EZShop</Navbar.Brand>
                     </LinkContainer>
                     <Nav className="ml-auto">
                         <LinkContainer to='/cart'>
-                            <Nav.Link><i className='fas fa-shopping-cart'></i>Sign in</Nav.Link>
+                            <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
                         </LinkContainer>
                         <LinkContainer to='/login'>
-                            <Nav.Link><i className='fas fa-user'></i>Cart</Nav.Link>
+                            <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
                         </LinkContainer>
                     </Nav>
                 </Container>
@@ -24,4 +25,9 @@ const Header = () => {
     )
 }
 
+
 export default Header
+
+
+
+
