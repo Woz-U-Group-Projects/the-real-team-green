e00fad1f91e0654a689798f92757dae5d79a7423
Steven Hamilton <shamilton7445@gmail.com>
2021-09-12 13:31:08 -0700

    modified:   .gitignore 	new file:   Procfile 	modified:   backend/server.js 	modified:   package.json

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,7 @@ if (process.env.NODE_ENV == 'development') {
 }
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("API is running")
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -31,6 +29,15 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is running")
+    })
+}
 
 
 const PORT = process.env.PORT || 5000
